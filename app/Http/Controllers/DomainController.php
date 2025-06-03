b29fd7889585337eb8ff21c370db1c5c7b5d0ff7
hugoblln <hugobellin@yahoo.com>
2025-06-03 15:45:09 +0200

    make controller, resource and request for Domain

---
+++ app/Http/Controllers/DomainController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Domain;
+use Illuminate\Http\Request;
+use App\Http\Resources\DomainResource;
+use App\Http\Requests\DomainCreateRequest;
+use App\Http\Requests\DomainUpdateRequest;
+
+class DomainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $domains = Domain::All();
+
+        return DomainResource::collection($domains);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(DomainCreateRequest $request)
+    {
+       $validatedData = $request->validated();
+
+       $domain = Domain::create($validatedData);
+
+       return response()->json([
+           'message' => 'Domain created successfully',
+           'domain' => new DomainResource($domain)
+       ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Domain $domain)
+    {
+        return new DomainResource($domain);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(DomainUpdateRequest $request, Domain $domain)
+    {
+       $validatedData = $request->validated();
+
+       $domaine->update($validatedData);
+
+       return response()->json([
+           'message' => 'Domain updated successfully',
+           'domain' => new DomainResource($domain)
+       ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Domain $domain)
+    {
+        $domain->forceDelete();
+
+        return response()->json([
+            'message' => 'Domain deleted successfully'
+        ]);
+    }
+}
